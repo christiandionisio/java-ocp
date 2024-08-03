8a9ec6906eb4cb13c19821136def2e0bcb1c9274
Christian Dionisio <christiandionisio9432@gmail.com>
2024-08-03 09:44:33 -0500

    SwitchStatements

---
+++ SwitchStatement.java
@@ -0,0 +1,35 @@
+public class SwitchStatement {
+
+    public static void main(String... args) {
+
+        // Without break statement in each branch executes
+        // all of the branches in the found order including default
+        int animal = 3;
+        switch(animal) {
+            case 1,2: System.out.print("Lion");
+            case 3:   System.out.print("Tiger");
+            // Notice putting a break statement it cuts execution
+            case 4:   System.out.print("Cat"); break;
+            default:   System.out.print("Leopard");
+        }
+        System.out.println("");
+        
+        // Before java 14 combining case values are like 
+        // this, (still compiling)
+        switch(animal) {
+            case 1: case 2: System.out.print("Lion");
+            case 3:         System.out.print("Tiger");
+            case 4:         System.out.print("Cat");
+            default:        System.out.print("Leopard");
+
+        }
+
+        // Values supported by switch statements:
+        // int, Integer, byte, Byte, short, Short,
+        // char, Character, String, enum values,
+        // var (only valid for preceding types)
+
+    }
+
+
+}
